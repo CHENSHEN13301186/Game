334a79f2d793918946a5443ee9137cc47aafc469
Shen Chen <shenc@tcd.ie>
2014-06-24 20:10:12 +0100

    Upload

---
+++ crystal-game-server/test/com/example/crystalgame/server/test/AllTests.java
@@ -0,0 +1,13 @@
+package com.example.crystalgame.server.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import com.example.crystalgame.server.datawarehouse.ServerDataWarehouse;
+
+@RunWith(Suite.class)
+@SuiteClasses({ ServerDataWarehouse.class })
+public class AllTests {
+
+}
